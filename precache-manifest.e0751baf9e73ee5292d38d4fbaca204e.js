0b616bc142e126eb5fe4ed440b325fc20ba49435
teonino <teonino3627@gmail.com>
2021-11-18 03:19:29 +0100

    Main build

---
+++ precache-manifest.e0751baf9e73ee5292d38d4fbaca204e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90ecdf5690e02e176b6",
+    "url": "/css/app.279bfbdf.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f94f17d5ac52d0495d4164c1227313c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cc22d619af22064149c",
+    "url": "/js/about.b7f1f916.js"
+  },
+  {
+    "revision": "c90ecdf5690e02e176b6",
+    "url": "/js/app.e75c9928.js"
+  },
+  {
+    "revision": "e306a9b5a2d6fe3e2457",
+    "url": "/js/chunk-vendors.4b6a23f5.js"
+  },
+  {
+    "revision": "9044440cc2c1abae95fb3e00371cb764",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
